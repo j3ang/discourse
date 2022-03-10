5a596fd6e317527f54256df17f50104c1924aef1
j3ang <alex@learningexecutive.com>
2022-03-10 15:47:00 -0800

    add class

---
+++ sync.rb
@@ -0,0 +1,22 @@
+require 'mysql2'
+
+class SyncScripts
+
+    def initialize
+        # @client = Mysql2::Client.new(
+        #     host: BB_PRESS_HOST,
+        #     username: BB_PRESS_USER,
+        #     database: BB_PRESS_DB,
+        #     password: BB_PRESS_PW,
+        # )
+    end
+
+    def execute
+        post_init
+    end
+
+    def post_init
+        puts "database connected!"
+    end
+
+end
