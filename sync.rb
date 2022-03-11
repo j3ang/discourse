8eec352182fe2f56f902083cb48d281ef74a9124
j3ang <alex@learningexecutive.com>
2022-03-10 17:30:12 -0800

    update format using envirnment variables in ruby

--- sync.rb
+++ sync.rb
@@ -6,10 +6,10 @@ class SyncScripts
 
     def initialize
         @client = Mysql2::Client.new(
-            host: DB_HOST,
-            username: DB_USER,
-            database: DB_DB,
-            password: DB_PW,
+            host: #{ENV['DB_HOST']},
+            username: #{ENV['DB_USER']},
+            database: #{ENV['DB_DB']},
+            password: #{ENV['DB_PW']},
         )
     end
 
